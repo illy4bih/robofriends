091b3e3e6fbcf98bd3a7ea56ea7dda55f5c60a7e
Adi <avrabac@gmail.com>
2018-05-21 16:41:22 +0200

    Test

---
+++ src/components/robots.js
@@ -0,0 +1,49 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Adi Vrabac",
+        username: "AdiV",
+        email: "avrabac@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Lejla Kobiljak-Vrabac",
+        username: "Leki",
+        email: "sdaf@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Omar Vrabac",
+        username: "Wufi",
+        email: "wufko@gmail.com"
+    },
+    {
+        id: 4,
+        name: "da fasdg",
+        username: "AFAsf",
+        email: "sadfasd@hotmail.com"
+    },
+    {
+        id: 5,
+        name: "Hundi Hundos",
+        username: "HuHu",
+        email: "HuHu@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Sejla Kobiljak",
+        username: "Seki",
+        email: "sejlak@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Armin Brutus",
+        username: "AArma",
+        email: "bruta@hotmail.com"
+    },
+    {
+        id: 8,
+        name: "Semic Omer",
+        username: "Omix",
+        email: "somer@yahoo.com"
+    }];
